91487930a19bce3b441cc7c9aff390f73f80adc7
‘Katy’ <katyinseattle@me.com>
2017-02-05 09:07:43 -0800

    Problem 2 function and scaffolding started

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
+function multiply(a,b) {
+  var sumArray = [(a*b), 'The product of ' + a + 'and' + b + 'is ' + (a*b) + "." ];
+
+  return sumArray;
+}
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(4,7);
